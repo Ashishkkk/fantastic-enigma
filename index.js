3cecc7cb68b0994ac43886487753f744016ba542
Ashishkkk <90038987+Ashishkkk@users.noreply.github.com>
2023-07-18 16:06:41 +0530

    landing page desing

---
+++ index.js
@@ -0,0 +1,22 @@
+function toggle() {
+    let showhide = document.getElementById('hamburger');
+    if (showhide.style.display === "block") {
+        showhide.style.display = "none";
+    } else {
+        showhide.style.display = "block";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
